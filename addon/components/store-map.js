cb76605239f0e2d4637eed496b3aee238b46e3d1
Steve Mitchell <SMitchell@groupon.com>
2015-07-16 18:30:06 -0500

    add store-map component

---
+++ addon/components/store-map.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import layout from '../templates/components/store-map';
+
+/**
+  To use this component in your app, add this to the template:
+
+  ```{{map-component}}```
+
+
+**/
+
+export default Ember.Component.extend({
+  layout: layout
+});
---
+++ app/components/store-map.js
@@ -0,0 +1,3 @@
+import storeMap from 'ember-cli-store-locator/components/store-map';
+
+export default storeMap;
